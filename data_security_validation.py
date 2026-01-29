ec4efb38f5626faa4f7fd5c6c14bb0a3dccf8bd2
Elie <n.elie@alustudent.com>
2026-01-29 12:24:18 +0200

    Created data_security_validation file & Added documentation above of what it will be doing.

---
+++ data_security_validation.py
@@ -0,0 +1,7 @@
+"""
+Data Extraction & Secure Validation System
+
+This program extracts structured data from raw text using regex patterns
+while implementing security measures to prevent malicious input from
+compromising the system and avoiding exposing sensitive data in logs or any output streams.
+"""
